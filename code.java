3d67cd22040373a48edc1a4a6fbdbfe311fbbf73
JordanDemartin <jordan-demartin@hotmail.fr>
2020-12-23 18:00:29 +0100

    Ajout class noeud, retrait de quelques autres truc

--- code.java
+++ code.java
@@ -2,11 +2,10 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
 class Player {
+
+    
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
@@ -14,30 +13,24 @@ class Player {
         int x_joueur[] = new int[4];
         int y_joueur[] = new int[4];
 
-        String[][] map = new String[30][20];
-        for(int i = 0 ; i < 20 ; i++){
-            for(int j = 0 ; j < 30 ; j++){
-                map[j][i] = "-";
-            }
-        }
-
-
+        char[][] map = initialisation_map(30,20);
         
-        String direction = "LEFT";
+        
 
         // game loop
         while (true) {
             int N = in.nextInt(); // total number of players (2 to 4).
             int P = in.nextInt(); // your player number (0 to 3).
-            
+            String direction = "Oooooskour!";
+
             for (int i = 0; i < N; i++) {
                 int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
                 int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
                 int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
                 int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
 
-                map[X0][Y0] = ""+i;
-                map[X1][Y1] = ""+i;
+                map[X0][Y0] = Character.forDigit(i,10);
+                map[X1][Y1] = Character.forDigit(i,10);
                 x_joueur[i] = X1;
                 y_joueur[i] = Y1;
             }
@@ -45,36 +38,58 @@ class Player {
                 x_joueur[i] = -1;
                 y_joueur[i] = -1;
             }
-            
+
+            //affiche la position de chaque joueur
             for(int i = 0; i < 4; i++){
                 System.err.println("tête du joueur "+ i +" : x="+x_joueur[i]+" y="+y_joueur[i]);
             }
 
-
-            joueur_plus_proche(x_joueur, y_joueur, P);
-            while(!direction_possilbe(map, direction, x_joueur[P], y_joueur[P])){
-
-                if(direction == "LEFT"){
-                    direction = "RIGHT";
-                }else if(direction == "RIGHT"){
-                    direction = "DOWN";
-                }else if(direction == "DOWN"){
-                    direction = "UP";
-                }else if(direction == "UP"){
-                    direction = "LEFT";
-                }
-                
+            int score_max = -1;
+            
+            int temp = score(map, x_joueur[P]-1, y_joueur[P]); //score gauche
+            System.err.println("score gauche : " + temp);
+            if( temp > score_max ){
+                score_max = temp;
+                direction = "LEFT";
+            }
+            temp = score(map, x_joueur[P]+1, y_joueur[P]); //score droite
+            System.err.println("score droite : " + temp);
+            if( temp > score_max ){
+                score_max = temp;
+                direction = "RIGHT";
+            }
+            temp = score(map, x_joueur[P], y_joueur[P]+1); //score bas
+            System.err.println("score bas : " + temp);
+            if( temp > score_max ){
+                score_max = temp;
+                direction = "DOWN";
+            }
+            temp = score(map, x_joueur[P], y_joueur[P]-1); //score haut
+            System.err.println("score haut : " + temp);
+            if( temp > score_max ){
+                score_max = temp;
+                direction = "UP";
             }
             
-            //System.err.println("direction : "+direction);
-
             affiche_map(map);
-            
-            System.out.println(direction); // A single line with UP, DOWN, LEFT or RIGHT
+
+            System.out.println(direction);
         }
     }
 
-    private static void affiche_map(String[][] map){
+    private static char[][] initialisation_map(int L, int H){
+        char[][] map = new char[L][H];
+
+        for(int i = 0 ; i < H ; i++){
+            for(int j = 0 ; j < L ; j++){
+                map[j][i] = '-';
+            }
+        }
+
+        return map;
+    }
+
+    private static void affiche_map(char[][] map){
         for(int i = 0 ; i < 20 ; i++){
             for(int j = 0 ; j < 30 ; j++){
                 System.err.print(map[j][i] + " ");
@@ -83,169 +98,76 @@ class Player {
         }
     }
 
-    private static int joueur_plus_proche(int[] x_joueur, int[] y_joueur, int num_joueur){
-        
-        int min = 400;
-        int num_joueur_min = 0;
 
-        for(int i = 0; i < 4; i++){
-            if(x_joueur[i] != -1 && y_joueur[i] != -1 && i != num_joueur){
-
-                int distance = Math.abs(x_joueur[i] - x_joueur[num_joueur]);
-                distance += Math.abs(y_joueur[i] - y_joueur[num_joueur]);
-
-                if(distance < min){
-                    min = distance;
-                    num_joueur_min = i;
-                }
-
-            }
+    private static int score(char[][] map, int X, int Y){
+        if(X < 0 || Y < 0 || X > 29 || Y > 19){
+            return -1;
         }
-        
-        System.err.println("min distance: le joueur "+num_joueur_min+" à "+min+" cases.");
-        return num_joueur_min;
+        if(map[X][Y] != '-'){
+            return -1;
+        }
+        int score = 0;
+        if( X-1 >= 0 && map[X-1][Y] == '-' ){
+            score++;
+        }if( Y-1 >= 0 && map[X][Y-1] == '-' ){
+            score++;
+        }if( X+1 <= 29 && map[X+1][Y] == '-' ){
+            score++;
+        }if( Y+1 <= 19 && map[X][Y+1] == '-' ){
+            score++;
+        }
+        return score;
     }
 
-    private static boolean direction_possilbe(String[][] map, String direction, int Xmoi, int Ymoi){
+}
 
-        //affiche_map(map);
-        //System.err.println("direction : "+direction);
-        //System.err.println("Xmoi : "+Xmoi);
-        //System.err.println("Ymoi : "+Ymoi);
+class Noeud{
+    public Noeud gauche;
+    public Noeud droite;
+    public Noeud bas;
+    public Noeud haut;
+    
+    public char[][] map;
+    public int[] x_joueur;
+    public int[] y_joueur;
+    public int depth;
+    public int joueur_concerner;
+    public int score = -1;
 
-        if(direction == "LEFT"){
-            if(Xmoi != 0){
-                if(map[Xmoi-1][Ymoi] == "-"){
-                    //System.err.println("je peux aller à " + direction);
-                    return true;
-                }else{
-                    //System.err.println("je peux pas aller à " + direction);
-                    return false;
-            }
-            }else{
-                //System.err.println("je peux pas aller à " + direction);
-                return false;
-            }
-        }
+    public Noeud(char[][] map, int[] x_joueur, int[] y_joueur, int depth, int joueur_concerner, int x_move, int y_move){
+        this.map = map;
+        this.x_joueur = x_joueur;
+        this.y_joueur = y_joueur;
+        this.depth = depth;
+        this.joueur_concerner = joueur_concerner;
 
-        if(direction == "RIGHT"){
-            if(Xmoi != 29){
-                if(map[Xmoi+1][Ymoi] == "-"){
-                    //System.err.println("je peux aller à " + direction);
-                    return true;
-                }else{
-                    //System.err.println("je peux pas aller à " + direction);
-                    return false;
-            }
-            }else{
-                //System.err.println("je peux pas aller à " + direction);
-                return false;
-            }
-        }
+        this.map[x_move][y_move] = Character.forDigit(joueur_concerner,10);
 
-        if(direction == "UP"){
-            if(Ymoi != 0){
-                if(map[Xmoi][Ymoi-1] == "-"){
-                    //System.err.println("je peux aller à " + direction);
-                    return true;
-                }else{
-                    //System.err.println("je peux pas aller à " + direction);
-                    return false;
-            }
-            }else{
-                //System.err.println("je peux pas aller à " + direction);
-                return false;
-            }
-        }
-
-        if(direction == "DOWN"){
-            if(Ymoi != 19){
-                if(map[Xmoi][Ymoi+1] == "-"){
-                    //System.err.println("je peux aller à " + direction);
-                    return true;
-                }else{
-                    //System.err.println("je peux pas aller à " + direction);
-                    return false;
-            }
-            }else{
-                //System.err.println("je peux pas aller à " + direction);
-                return false;
-            }
-        }
-
-        return false;
+        this.create_childs();
     }
 
-    private static int score(String[][] map_copie, int[] x_joueur, int[] y_joueur, int num_joueur, int N){
-        String terminer[] = new String[N];
-        int nb_case_controler[] = new int[N];
-
-        for(int i = 0 ; i < N ; i++){
-            terminer[i] = "-";
+    public void create_childs(){
+        int nouveau_joueur_concerner = (this.joueur_concerner+1)%4;
+        if( this.x_joueur[nouveau_joueur_concerner] != 0 ){
+            if( map[this.x_joueur[nouveau_joueur_concerner]-1][this.y_joueur[nouveau_joueur_concerner]] == '-'){
+                this.gauche = new Noeud( this.map.clone(), this.x_joueur, this.y_joueur, this.depth, nouveau_joueur_concerner, this.x_joueur[nouveau_joueur_concerner]-1, this.y_joueur[nouveau_joueur_concerner]);
+            }
         }
-        
-        for(int i = 0; i < N; i++){
-            if(x_joueur[i] == -1 || y_joueur[i] == -1){
-                terminer[i] = "fin";
+        if( this.y_joueur[nouveau_joueur_concerner] != 0 ){
+            if( map[this.x_joueur[nouveau_joueur_concerner]][this.y_joueur[nouveau_joueur_concerner]-1] == '-'){
+                this.haut = new Noeud( this.map.clone(), this.x_joueur, this.y_joueur, this.depth, nouveau_joueur_concerner, this.x_joueur[nouveau_joueur_concerner], this.y_joueur[nouveau_joueur_concerner]-1);
             }
-
-            if(x_joueur[i] != 0){
-                if( map_copie[x_joueur[i]-1][y_joueur[i]] == "-"){
-                    map_copie[x_joueur[i]-1][y_joueur[i]] = "i";
-                }
-            }
-
-            if(y_joueur[i] != 0){
-                if( map_copie[x_joueur[i]][y_joueur[i]-1] == "-"){
-                    map_copie[x_joueur[i]][y_joueur[i]-1] = "i";
-                }
-            }
-
-            if(x_joueur[i] != 29){
-                if( map_copie[x_joueur[i]+1][y_joueur[i]] == "-"){
-                    map_copie[x_joueur[i]+1][y_joueur[i]] = "i";
-                }
-            }
-
-            if(y_joueur[i] != 19){
-                if( map_copie[x_joueur[i]][y_joueur[i]+1] == "-"){
-                    map_copie[x_joueur[i]][y_joueur[i]+1] = "i";
-                }
-            }
-
         }
-        return 0;
+        if( this.x_joueur[nouveau_joueur_concerner] != 29 ){
+            if( map[this.x_joueur[nouveau_joueur_concerner]+1][this.y_joueur[nouveau_joueur_concerner]] == '-'){
+                this.droite = new Noeud( this.map.clone(), this.x_joueur, this.y_joueur, this.depth, nouveau_joueur_concerner, this.x_joueur[nouveau_joueur_concerner], this.y_joueur[nouveau_joueur_concerner]);
+            }
+        }
+        if( this.y_joueur[nouveau_joueur_concerner] != 19 ){
+            if( map[this.x_joueur[nouveau_joueur_concerner]+1][this.y_joueur[nouveau_joueur_concerner]] == '-'){
+                this.bas = new Noeud( this.map.clone(), this.x_joueur, this.y_joueur, this.depth, nouveau_joueur_concerner, this.x_joueur[nouveau_joueur_concerner], this.y_joueur[nouveau_joueur_concerner]-1);
+            }
+        }
     }
 
-    private static String[][] remplis_map_copie(String[][] map_copie, int x, int y, int numero_joueur){
-        
-        if(x != 0){
-            if( map_copie[x-1][y] == "-"){
-                map_copie[x-1][y] = "p" + numero_joueur;
-                map_copie = remplis_map_copie(map_copie, x-1, y, numero_joueur);
-            }
-        }
-
-        if(y != 0){
-            if( map_copie[x][y-1] == "-"){
-                map_copie[x][y-1] = "p" + numero_joueur;
-                map_copie = remplis_map_copie(map_copie, x, y-1, numero_joueur);
-            }
-        }
-
-        if(x != 29){
-            if( map_copie[x+1][y] == "-"){
-                map_copie[x+1][y] = "p" + numero_joueur;
-                map_copie = remplis_map_copie(map_copie, x+1, y, numero_joueur);
-            }
-        }
-
-        if(y != 19){
-            if( map_copie[x][y+1] == "-"){
-                map_copie[x][y+1] = "p" + numero_joueur;
-                map_copie = remplis_map_copie(map_copie, x, y+1, numero_joueur);
-            }
-        }
-        return map_copie;
-    }
 }
